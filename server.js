ca9711d383858de0bd2603654ef2e8bd486c2c1c
UPye <Urika.Pye@gmail.com>
2021-07-08 10:39:50 -0400

    Change port to see if deploy issues resolved. App works locally, but not when heroku deployed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budgettracker";
 
 const app = express();
