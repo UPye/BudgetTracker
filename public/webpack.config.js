367f2f4da34aa8b898e5593d010410d8e54aef58
UPye <Urika.Pye@gmail.com>
2021-07-08 10:14:05 -0400

    Changed how manifest.json is linked to index.html file. PLEASE WORK

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const config = {
     entry: {
-      app: './public/js/index.js',
+      app: '../public/js/index.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -48,14 +48,14 @@ const config = {
         name: "Budget_Tracker",
         short_name: "BudgetTracker",
         description: "An app that allows you to keep track of your budget.",
-        start_url: '../index.html',
+        start_url: './index.html',
         background_color: '#01579b',
         theme_color: '#ffffff',
         fingerprints: false,
         inject: false,
         icons: [
           {
-            src: path.resolve('public/icons'),
+            src: path.resolve('./icons'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons')
           }
